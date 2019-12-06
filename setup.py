9b6481932eb4a4c35e7b629e529e171cb250e5a9
Mohamed Yousif <mmbusif@gmail.com>
2019-12-06 06:31:34 +0200

    bip44 name

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='bip44',
+    version='0.1',
+    packages=['bip44',],
+    license='Attributes to https://github.com/michailbrynard/ethereum-bip44-python',
+    long_description="A package friendly version of michailbryanard work.",
+)
